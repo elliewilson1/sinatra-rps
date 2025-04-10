16f80c60943411c0dbf37880d176427aa5a36916
elliewilson1 <ewilson0@chicagobooth.edu>
2025-04-10 03:01:24 +0000

    ready to test

--- app.rb
+++ app.rb
@@ -2,8 +2,61 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  erb(:home)
+
+end
+
+get("/Rock") do
+
+  moves = ["rock", "paper", "scissors"]
+
+  @their_move = moves.sample
+
+  if @their_move == "rock"
+    @outcome = "tied"
+  elsif @their_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+  erb(:rock)
+
+end
+
+get("/Paper") do
+
+  moves = ["rock", "paper", "scissors"]
+
+  @their_move = moves.sample
+
+  if @their_move == "rock"
+    @outcome = "won"
+  elsif @their_move == "paper"
+    @outcome = "tied"
+  else
+    @outcome = "lost"
+  end
+
+  erb(:paper)
+
+end
+
+get("/Scissors") do
+
+  moves = ["rock", "paper", "scissors"]
+
+  @their_move = moves.sample
+
+  if @their_move == "rock"
+    @outcome = "lost"
+  elsif @their_move == "paper"
+    @outcome = "won"
+  else
+    @outcome = "tied"
+  end
+
+  erb(:scissors)
+
 end
